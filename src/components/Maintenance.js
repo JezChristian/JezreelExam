2aefc585f4b142eaf6b1a0aa690c2d2819589634
unknown <bacasnojezreel@gmail.com>
2023-07-10 02:43:25 +0800

    This is the Finished Product of my exam - Jezreel Christian Bacasno

---
+++ src/components/Maintenance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../scss/App.scss"
+
+function Maintenance() {
+  return (
+    <div className='Maintenance'>
+        <h1>
+            404 Sorry this page is under maintenance...
+        </h1>
+    </div>
+  )
+}
+
+export default Maintenance
